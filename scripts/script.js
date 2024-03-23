316bdca8e2b9aca6b93906e032a60c0967a35335
Gaabriel Ponomarjov <157696718+xefech@users.noreply.github.com>
2024-03-23 22:14:17 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const myImage_src = myImage.getAttribute("src");
+
+  if (myImage_src === "images/audi.jpg") {
+    myImage.setAttribute("src", "images/audi2.jpg")
+  }
+  else {
+    myImage.setAttribute("src", "images/audi.jpg")
+  }
+}
